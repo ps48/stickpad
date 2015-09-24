beb1717b89d44d36bb65d55ddbefdefa03e4037f
unknown <pshenoy36@gmail.com>
2015-09-24 13:53:16 +0530

    UI tweak css anim

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 $(document).ready(function(){
 
-	//if(localStorage.getItem("con"))
-///	{
-	//	$('#conheader').append(localStorage.getItem("con"));
-//	}
+	if(localStorage.getItem("con"))
+	{
+		$('#conheader').append(localStorage.getItem("con"));
+	}
 
 	var x=1;
-	var col=['#e77e23','#27ae61','#2a80b9','#d64a4b','#f39c11'];
+	var col=['#3498db','#2ecc71','#e67e22','#c0392b','#2c3e50','#d35400','#e74c3c','#1abc9c','#2c3e50','#7f8c8d'];
 	
 	$('input').keypress(function (e) {
 	  if (e.which == 13) {
 
-	  	var rno = Math.floor(Math.random()*(5));
+	  	var rno = Math.floor(Math.random()*(10));
+
 	  	var txt="<div id=x"+x+" class='cardholder dre' draggable='true' ondragstart='drag(event)' style='background-color:"
 	  			+col[rno]+"' >"+$('#usr').val()+"<button class='pre' id='"+x+"' onclick='func(this.id)' style='background-color:"
 	  			+col[rno]+"; border:transparent!important;'>x</button></div>";
+
 	    $('#conheader').append(txt);
-	     $("#usr").val("");
-	   // var temp=localStorage.getItem("con");
-	    // localStorage.setItem("con",temp+txt );
+	    $('#x'+x).addClass('animated bounceInLeft');
+	    $("#usr").val("");
+
+	    var temp=localStorage.getItem("con");
+	    localStorage.setItem("con",temp+txt );
+
 	     x=x+1;
 	    return false;  
 
@@ -32,8 +37,9 @@ $(document).ready(function(){
 
 function func(x){
 
-		var element = document.getElementById("x"+x);
-		element.parentNode.removeChild(element);
+		$('#x'+x).addClass('animated flipOutX').remove();
+
+
 	}
 
 function allowDrop(ev) {
